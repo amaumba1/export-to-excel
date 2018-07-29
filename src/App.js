e621dcd0a714124eacfe33c17ec18154f6675082
Ally Maumba <amaumba1@gmail.com>
2018-07-29 01:01:08 -0500

    added React-table

--- src/App.js
+++ src/App.js
@@ -1,32 +1,66 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+import ReactTable from 'react-table'; 
+import 'react-table/react-table.css'; 
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [
+        {
+          firstname: 'Allen',
+          lastname: 'John',
+          age: '65'
+        },
+        {
+          firstname: 'Smith',
+          lastname: 'David',
+          age: '53'
+        }
+      ]
+    }
+  }
   render() {
+    const userColumns = [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            id: 'firstname',
+            accessor: d => d.firstname
+          },
+          {
+            Header: 'Last Name',
+            id: 'lastname',
+            accessor: d => d.lastname
+          }
+        ]
+      },
+      {
+        Header: 'Age',
+        columns: [
+          {
+            Header: 'Age',
+            id: 'age',
+            accessor: d => d.age
+          }
+        ]
+      }
+    ]
     return (
       <div style={style}>
         <div>
-        <h1>Demo</h1> 
-          <button>Export to Excel</button> 
-          <table>
-            <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Age</th>
-            </tr>
-            <tr>
-              <th>John</th>
-              <th>Smith</th>
-              <th>49</th>
-            </tr>
-            <tr>
-              <th>David</th>
-              <th>Mark</th>
-              <th>34</th>
-            </tr>
-          </table> 
-      </div>
+          <h1>Demo</h1> 
+            <button>Export to Excel</button>
+            <ReactTable 
+              style={{marginLeft: '-40%', marginRight: '-40%',}}
+              data={this.state.users}
+              columns={userColumns}
+            /> 
+        </div>
       </div>
     );
   }
